af50264ce7ca522bfc155f0d3a9c9403c49668a9
Edison Devadoss <edisonj1996@gmail.com>
2017-10-23 11:49:42 +0530

    Firebase Google Auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,22 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { GooglePlus } from '@ionic-native/google-plus';
+import { AngularFireModule } from 'angularfire2';
+import * as firebase from "firebase";
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAOjn6dyYXHL8jo8FB3IOy1DyO178ZgKeo",
+  authDomain: "facebookauth-787d0.firebaseapp.com",
+  databaseURL: "https://facebookauth-787d0.firebaseio.com",
+  projectId: "facebookauth-787d0",
+  storageBucket: "facebookauth-787d0.appspot.com",
+  messagingSenderId: "806510472483"
+}
+firebase.initializeApp(firebaseConfig);
 
 @NgModule({
   declarations: [
@@ -14,7 +28,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,6 +39,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    GooglePlus,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { GooglePlus } from '@ionic-native/google-plus';
+import { AngularFireModule } from 'angularfire2';
+import * as firebase from "firebase";
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private googlePlus: GooglePlus) {
 
+  }
+  login(){
+    this.googlePlus.login({
+      'webClientId':'806510472483-q4gmkrkthp7kqu7e4qi61r8qncbvupkc.apps.googleusercontent.com',
+      'offline': true
+    })
+    .then((res)=>{
+      firebase.auth().signInWithCredential(firebase.auth.GoogleAuthProvider.credential(res.idToken))
+      .then((succ)=>{
+        console.log(succ);
+        alert('Login Sucess')
+      })
+      .catch((err)=>{
+        alert('Login Err')
+        console.error(err);
+      })
+    })
   }
 
 }
